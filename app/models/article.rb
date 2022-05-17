d06bc395e7b3e65923466e9338f14a116d2051ef
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-17 02:54:08 -0700

    Add a custom validator in the model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
+class MyValidator<ActiveModel::Validator
+  def validate(record)
+    unless record.title.start_with?'D'
+      record.errors.add :title,"Need a name starting with D please"
+    end
+  end
+end
 class Article < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with MyValidator
   include Visible
   has_many :comments, dependent: :destroy
   validates :title, presence: true
