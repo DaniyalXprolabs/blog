1e68114bf8b9290456f20b0846aeb6e860fa9e8d
DaniyalXprolabs <daniyal.malik@xprolabs.com>
2022-05-17 05:26:22 -0700

    Callbacks are working correctly and efficently

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,11 +6,19 @@ class MyValidator<ActiveModel::Validator
   end
 end
 class Article < ApplicationRecord
-  include ActiveModel::Validations
-  validates_with MyValidator
+  #include ActiveModel::Validations
+  #validates_with MyValidator
   include Visible
   has_many :comments, dependent: :destroy
   validates_associated :comments
-  validates :title, presence: {message:"Please give the title of article it is necassary"}
+  #before_validation :set_title
+  #after_validation :set_title
+  before_save :set_title
+  validates :title, presence: { message:"Please enter a title" }
   validates :body, presence: true, length: {minimum: 10}
+
+  private
+  def set_title
+    self.title = "Pay electricity bill and TV Bill"
+  end
 end
